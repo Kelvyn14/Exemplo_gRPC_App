c0ea4e751c5167539aed4937770e8d67890fb21f
Kelvyn Araujo <Kelvynra@gmail.com>
2023-03-28 19:41:47 -0300

    Inclusão de projetos de exemplo de Client + Server para aplicações utilizando gRPC

---
+++ Grpc_App_Client/Grpc_App1_Client/Program.cs
@@ -0,0 +1,15 @@
+﻿using Grpc.Net.Client;
+using Grpc_App1_Client;
+
+
+// The port number must match the port of the gRPC server.
+using var channel = GrpcChannel.ForAddress("https://localhost:7039");
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
+
+Console.WriteLine("Greeting: " + reply.Message);
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
+
+// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
---
+++ Grpc_App_Server/Grpc_App_1/Services/GreeterService.cs
@@ -0,0 +1,22 @@
+using Grpc.Core;
+using Grpc_App_1;
+
+namespace Grpc_App_1.Services
+{
+    public class GreeterService : Greeter.GreeterBase
+    {
+        private readonly ILogger<GreeterService> _logger;
+        public GreeterService(ILogger<GreeterService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply
+            {
+                Message = "Hello " + request.Name + ", It's a beautiful day to learn gRPC!!!"
+            });
+        }
+    }
+}
